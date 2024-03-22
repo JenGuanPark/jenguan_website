ebf0ce30debc7cd306f60e3cfa68814194cdf92e
JenGuanPark <zhenguang0203@gmail.com>
2024-03-22 11:02:45 +0800

    02_hardhat-deploy

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -2,13 +2,11 @@ export const WETH = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"; // Mainnet
 
 // Metadata Head
 export const metadata = {
-  title: "JenGuan_web3",
-  description:
-    "Next.js dApp template with Tailwind CSS and RainbowKit built in TypeScript.",
-  keywords:
-    "React, Next.js, Tailwind CSS, Sass, RainbowKit, Solidity, Hardhat, ethers.js, Blockchain, TypeScript",
+  title: "JenGuan_Web3",
+  description: "JenGuan_website",
+  keywords: "web3, Blockchain, BTC, Dapp",
   type: "website",
   url: "next-dapp-matteocelani.vercel.app",
   image: "next-dapp-matteocelani.vercel.app/img/logo.png",
-  autor: "Matteo Celani",
+  autor: "JenGuan Park",
 };
